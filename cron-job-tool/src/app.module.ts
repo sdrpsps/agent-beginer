1753dc626710aba44e7c32548ee4380e6cb7cfb3
sunny <sunny@bytespark.app>
2026-04-15 16:25:28 +0800

    数据库查询和定时任务

--- cron-job-tool/src/app.module.ts
+++ cron-job-tool/src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Inject, Module, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule, SchedulerRegistry } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'node:path';
+import { AiModule } from './ai/ai.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AiModule } from './ai/ai.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'node:path';
+import { JobModule } from './job/job.module';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+import { Job } from './job/entities/job.entity';
 
 @Module({
   imports: [
@@ -31,8 +37,48 @@ import { join } from 'node:path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        connectorPackage: 'mysql2',
+        logging: true,
+        entities: [User, Job],
+      }),
+    }),
+    UsersModule,
+    ScheduleModule.forRoot(),
+    JobModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  @Inject(SchedulerRegistry)
+  schedulerRegistry: SchedulerRegistry;
+
+  onApplicationBootstrap() {
+    // const job = new CronJob(CronExpression.EVERY_SECOND, () => {
+    //   console.log('run job');
+    // });
+    // this.schedulerRegistry.addCronJob('job1', job);
+    // job.start();
+    // setTimeout(() => {
+    //   this.schedulerRegistry.deleteCronJob('job1');
+    // }, 5000);
+    // const intervalRef = setInterval(() => {
+    //   console.log('run interval job');
+    // }, 1000);
+    // this.schedulerRegistry.addInterval('interval1', intervalRef);
+    // setTimeout(() => {
+    //   this.schedulerRegistry.deleteInterval('interval1');
+    // }, 5000);
+  }
+}
